cf70d76026713f2e8d9b053b73bd904a457dc90b
AliciaLv <75115651+AliciaLv@users.noreply.github.com>
2021-03-31 10:52:25 +0200

    view_log

---
+++ views/login.php
@@ -0,0 +1,7 @@
+<section>
+    <form action="" method="POST">
+        <input type="text" name="nomU" placeholder="Nom d'utilisateur">
+        <input type="password" name="mdp" placeholder="Mot de passe">
+        <input type="submit" name="valider" value="Se connecter">
+    </form>
+</section>
